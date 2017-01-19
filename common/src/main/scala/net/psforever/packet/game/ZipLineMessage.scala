61e2618a9524b2e1082652ac3a8f612646d1a4da
FateJH <Jason_DiDonato@yahoo.com>
2017-01-18 20:56:35 -0500

    updated handling of the three Long values

--- common/src/main/scala/net/psforever/packet/game/ZipLineMessage.scala
+++ common/src/main/scala/net/psforever/packet/game/ZipLineMessage.scala
@@ -1,16 +1,18 @@
 // Copyright (c) 2016 PSForever.net to present
 package net.psforever.packet.game
 
+import net.psforever.newcodecs.newcodecs
 import net.psforever.packet.{GamePacketOpcode, Marshallable, PlanetSideGamePacket}
 import scodec.Codec
 import scodec.codecs._
+import shapeless.{::, HNil}
 
 /**
   * The player is interacting with a zipline.<br>
   * <br>
-  * Action:
-  * `0 - Attach to zipline`
-  * `1 - Arrived at destination`
+  * Action:<br>
+  * `0 - Attach to zipline`<br>
+  * `1 - Arrived at destination`<br>
   * `2 - Forcibly detach from zipline in mid-transit`
   * @param player_guid the player
   * @param origin_side whether this corresponds with the "entry" or the "exit" of the zipline, as per the direction of the light pulse visuals
@@ -40,13 +42,37 @@ final case class ZipLineMessage(player_guid : PlanetSideGUID,
 }
 
 object ZipLineMessage extends Marshallable[ZipLineMessage] {
-  implicit val codec : Codec[ZipLineMessage] = (
-    ("player_guid" | PlanetSideGUID.codec) ::
-      ("origin_side" | bool) ::
-      ("action" | uint2L) ::
-      ("guid" | uint32L) ::
-      ("unk3" | uint32L) ::
+  type threeLongsPattern = Long :: Long :: Long :: HNil
+
+  /**
+    * A `Codec` for when three `Long` values are to be read or written.
+    */
+  val threeLongValues : Codec[threeLongsPattern] = (
+    ("unk3" | uint32L) ::
       ("unk4" | uint32L) ::
       ("unk5" | uint32L)
+    ).as[threeLongsPattern]
+
+  /**
+    * A `Codec` for when there are no extra `Long` values are present.
+    */
+  val noLongValues : Codec[threeLongsPattern] = ignore(0).xmap[threeLongsPattern] (
+    {
+      case _ =>
+        0L :: 0L :: 0L :: HNil
+    },
+    {
+      case _ =>
+        ()
+    }
+  )
+
+  implicit val codec : Codec[ZipLineMessage] = (
+    ("player_guid" | PlanetSideGUID.codec) >>:~ { player =>
+      ("origin_side" | bool) ::
+        ("action" | uint2) ::
+        ("id" | uint32L) ::
+        newcodecs.binary_choice(player.guid > 0, threeLongValues, noLongValues) // !(player.guid == 0)
+    }
     ).as[ZipLineMessage]
 }
